7c9afa2b69ec48754b74e2f17d264d3abf8e1b02
mcenahle <hi@mcenahle.page>
2026-07-31 20:19:45 +0800

    init: 网站初始化，加入一个卡片
    可能是一个长期维护的 vue + element plus 网站...

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <el-card>
+    <template #header> Mcenahle </template>
+    <p>欢迎来到 <code>i.mcenahle.page</code>！我是一名安静的大专男生。</p>
+    <p>个人爱好：计算机代码、医学知识和自己的闲暇生活。</p>
+  </el-card>
+</template>
